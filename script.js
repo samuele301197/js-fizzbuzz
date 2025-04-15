d6fbd9dfc3bf9b7ca8e29bfe61a419b26e3d5651
samuele301197 <parisisamu32@gmail.com>
2025-04-15 16:40:58 +0200

    feat:commento

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Ciò che vogliamo è che il programma stampi i numeri divisibili per 3(fizz), per 5(buzz), e sia per 3 che per 5(fizzbuzz). Per cui per prima cosa andiamo a richiedere l'informazione con più contenuti, impostandola su IF, in seguito gli "scarti" dell'informazione li mettiamo su else if (grazie a Loris per la delucidazione!)
+
+
 for (let i = 1; i <= 100; i++) {
     console .log("iterazione", i);
     if ((i % 3 === 0 && i % 5 === 0)) {
