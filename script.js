d9b23cb021c1d15a4480e3f72017bf3f05be0c77
samuele301197 <parisisamu32@gmail.com>
2025-04-15 15:51:11 +0200

    feat:fizzbuzz

---
+++ script.js
@@ -0,0 +1,10 @@
+for (let i = 1 <= 100; i++;) {
+    console .log("iterazione", i);
+    if (i % 3 === 0) {
+        console.log("FIZZ");
+    } if else ( i % 5 === 0) {
+        console.log("BUZZ");
+    } else ( i % 5 === 0 && i % 3 === 0) {
+        console .log("FIZZBUZZ");
+    }
+}
